1406aeb6f33d74b5fef45bd6fb22ed70fce941ab
Vasanth Sarathy <vsarathy@sift.net>
2024-01-07 21:18:13 -0500

    Made some UI updates

--- magnum/magnum/magnum.py
+++ magnum/magnum/magnum.py
@@ -14,24 +14,65 @@ class ParseState(rx.State):
 
 
 def header() -> rx.Component:
-    return rx.text("header")
+    return rx.box(
+        rx.spacer(),
+        rx.text("🐧 OPUS", text_align="justify", font_family="Courier New", font_size="3em"),
+        rx.text("Open World Natural Language Parser with Unrestricted Semantics", font_family="Courier New"),
+        rx.spacer(),
+        rx.html("<br>"),
+        rx.divider(),
+        align_items="left",
+        padding_left="2em",
+        padding_right="2em",
+        padding_top="2em",
+        padding_below="2em"
+    )
 
 def input_area() -> rx.Component:
-    return rx.text("Input area")
+    return rx.box(
+            rx.hstack(
+                rx.input(),
+                rx.button("Parse")
+            ),
+            align_items="left",
+        padding_left="2em",
+
+    )
 
 def output_area() -> rx.Component:
-    return rx.text("Output area")
+    return rx.tabs(
+    rx.tab_list(
+        rx.tab("Trade Semantics"),
+        rx.tab("Surface Meaning Representation (SMR)"),
+    ),
+    rx.tab_panels(
+        rx.tab_panel(rx.text("Text from tab 1.")),
+        rx.tab_panel(rx.checkbox("Text from tab 2.")),
+    ),
+    bg="white",
+    color="black",
+    shadow="lg",
+    padding_right="2em"
+)
 
 def edit_area() -> rx.Component:
-    return rx.text("Edit area")
+    return rx.card(
+    rx.text("Body of the Card Component"),
+    header=rx.heading("Header", size="lg"),
+    footer=rx.heading("Footer", size="sm"),
+    padding_left="2em",
+        padding_right="2em",
+)
 
 
 def index() -> rx.Component:
     return rx.grid(
-        rx.grid_item(header(), row_span=1, col_span=4, bg="lightblue"),
-        rx.grid_item(input_area(), row_span=2, col_span=2, bg="lightgreen"),
-        rx.grid_item(output_area(), row_span=1, col_span=2, bg="lightgreen"),
-        rx.grid_item(edit_area(), row_span=4, col_span=4, bg="orange"),
+        rx.grid_item(header(), row_span=1, col_span=4, bg="white"),
+        rx.grid_item(input_area(), row_span=2, col_span=2, bg="white"),
+        rx.grid_item(output_area(), row_span=1, col_span=2, bg="white", padding_left="2em",
+        padding_right="2em"),
+        rx.grid_item(edit_area(), row_span=4, col_span=4, bg="white", padding_left="2em",
+        padding_right="2em"),
         template_rows="repeat(7, 1fr)",
         template_columns="repeat(4, 1fr)",
         h="200px",
